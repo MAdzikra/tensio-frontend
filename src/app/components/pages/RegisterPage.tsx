2e8b357ff101e46ba43f3376e724ba8d6131e67d
Muhammad Adzikra Dhiya Alfauzan <112993562+MAdzikra@users.noreply.github.com>
2026-05-12 20:47:30 +0700

    Change button text from 'continue_with' to 'signup_with'

--- src/app/components/pages/RegisterPage.tsx
+++ src/app/components/pages/RegisterPage.tsx
@@ -139,9 +139,11 @@ export function RegisterPage() {
               onError={() => {
                 alert("Google Login Failed");
               }}
-              text='continue_with'
+              text='signup_with'
               logo_alignment='center'
               shape='pill'
+              size='large'
+              width='100%'
             />
           </div>
 
